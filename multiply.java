84027114a24e2a3114e8871de81f0d7fb33a0828
Venkata Narsimha Reddy <nvnreddynaini@gmail.com>
2022-01-11 20:48:07 +0530

    added multiply.java

--- multiply.java
+++ multiply.java
@@ -4,6 +4,6 @@ public class multiply {
             int a = 5;
             int b = 6;
     
-            System.out.println("The sum is " + (a+b));
+            System.out.println("The sum is " + (a*b));
         }
 }
